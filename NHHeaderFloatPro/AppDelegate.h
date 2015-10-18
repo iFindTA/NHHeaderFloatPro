491d95842f6a5bedc029596bf5124819185fc48b
hu jiaju <nanhujiaju@163.com>
2015-10-18 13:28:55 +0800

    add new files

---
+++ NHHeaderFloatPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NHHeaderFloatPro
+//
+//  Created by hu jiaju on 15-10-18.
+//  Copyright (c) 2015年 Nanhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NHHeaderFloatPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NHHeaderFloatPro
+//
+//  Created by hu jiaju on 15-10-18.
+//  Copyright (c) 2015年 Nanhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
